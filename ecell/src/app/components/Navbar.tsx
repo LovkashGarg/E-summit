48c3cc79585f82a2c084667d885ec5734215ac86
Nikhil <nikhilprajapati1411@gmail.com>
2024-09-14 15:47:38 +0530

    changes done

--- ecell/src/app/components/Navbar.tsx
+++ ecell/src/app/components/Navbar.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { Button } from "./ui/moving-border";
 import Image from "next/image";
 import logo from "../../../public/courses/E-Cell logo Yellow-white.png"
+import { HoverBorderGradientDemo } from "./HoverButton";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
 function Navbar({ className }: { className?: string }) {
 
   const [active, setActive] = useState<string | null>(null);
@@ -34,8 +36,16 @@ function Navbar({ className }: { className?: string }) {
 </div>
 
       <div>
-      <Button borderRadius="1.75rem" >Login </Button>
-      <Button borderRadius="1.75rem" >Sign Up </Button>
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2">
+        <AceternityLogo />
+        <span>Aceternity UI</span>
+      </HoverBorderGradient>
+      {/* <HoverBorderGradientDemo/> */}
+
+
       </div>
     </div>
       </div>
@@ -43,4 +53,23 @@ function Navbar({ className }: { className?: string }) {
   )
 }
 
+
+const AceternityLogo = () => {
+  return (
+    (<svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-3 w-3 text-black dark:text-white">
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round" />
+    </svg>)
+  );
+};
 export default Navbar;
